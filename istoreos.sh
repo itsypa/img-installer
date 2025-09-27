87cd5436e41221db3cd17fd84176d80254e954a8
itsypa <132572291+itsypa@users.noreply.github.com>
2025-09-27 23:47:38 +0800

    Update istoreos.sh

--- istoreos.sh
+++ istoreos.sh
@@ -2,10 +2,10 @@
 mkdir -p openwrt
 
 REPO="wukongdaily/img-installer"
-TAG="2025-03-12"
-FILE_NAME="istoreos-24.10.1-2025060614-x86-64-squashfs-combined-efi.img.gz"
+TAG="2025-09-26"
+FILE_NAME="istoreos-24.10.2.img.gz"
 OUTPUT_PATH="openwrt/istoreos.img.gz"
-DOWNLOAD_URL=$(curl -s https://api.github.com/repos/$REPO/releases/tags/$TAG | jq -r '.assets[] | select(.name == "'"$FILE_NAME"'") | .browser_download_url')
+DOWNLOAD_URL="https://github.com/itsypa/img-installer/releases/download/iStoreOS-24.10.2/istoreos-24.10.2.img.gz"
 
 if [[ -z "$DOWNLOAD_URL" ]]; then
   echo "错误：未找到文件 $FILE_NAME"
